171b874a8117c627fdca48231bdc45e1c3250b92
seregafedotov06-ux <seregafedotov06@gmail.com>
2026-01-14 21:11:22 +0500

    Commit 2

--- lab5.cpp
+++ lab5.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
+void readFile(string fname, vector<char> s) {
+
+};
+
+void printStr(vector<char> s) {
+
+};
+
+void writeStr(vector<char> s) {
+
+};
 
 int main() {
-    cout << "Hello World!  " << endl;
+    vector<char> fStr;
+    readFile("file.txt", fStr);
+    printStr(fStr);
+    writeStr(fStr);
 }
 
