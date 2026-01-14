32d8411965a06a30b3e13d8faaf70367e81acf7a
seregafedotov06-ux <seregafedotov06@gmail.com>
2026-01-14 21:27:11 +0500

    Commit 3

--- lab5.cpp
+++ lab5.cpp
@@ -5,22 +5,30 @@
 
 using namespace std;
 
-void readFile(string fname, vector<char> s) {
+void readFile(const string& fname, vector<string>& s) {
 
 };
 
-void printStr(vector<char> s) {
-
+void printStr(const vector<string>& s) {
+    for (int i=0; i<s.size(); ++i) {
+        cout << s[i] <<endl;
+    }
 };
 
-void writeStr(vector<char> s) {
-
+void writeStr(const vector<string>& s) {
+    ofstream file("newfile.txt", ios::binary);
+    if (!file) {
+        cerr << "Ошибка открытия файла!" << endl;
+    }
+    for (int i=0; i<s.size(); ++i) {
+        file << s[i] <<endl;
+    }
+    file.close();
 };
 
 int main() {
-    vector<char> fStr;
-    readFile("file.txt", fStr);
+    vector<string> fStr;
+    readFile("tv.txt", fStr);
     printStr(fStr);
     writeStr(fStr);
 }
-
